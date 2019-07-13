11e7c60c60df9f640c806b6db5ec2e501cf12648
Alexander Shelopukho <AShelopukho@veeam.com>
2019-07-13 09:53:13 +0200

    changed calendar seo description

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="Календарь соревнований, пробегов, тренировок" />
         <Bio />
         <table className="calendar">
           <thead>
